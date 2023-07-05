8b713a0f163b1e1dca6282fa9f65fc55e8dbd9ac
Naman Jain <120260121+shubhamjainnn@users.noreply.github.com>
2023-07-05 12:08:46 +0530

    not done

---
+++ app.js
@@ -0,0 +1,4 @@
+const billAmounnt = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+console.log(cashGiven.value);
